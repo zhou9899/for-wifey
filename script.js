cefce22456edb29d85b73b6ba956cf5cb5ec6289
Zhou9899 <your_email@example.com>
2025-12-24 12:18:40 +0500

    Birthday message 💖

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const intro = document.getElementById("intro");
 const letter = document.getElementById("letter");
 const final = document.getElementById("final");
@@ -7,19 +8,18 @@ const bg = document.getElementById("bg");
 const letterText = document.getElementById("letterText");
 
 const message = `
-My Strawberry 🍓, my wifey, my Ryuberry…
+Hey Wifey ... 🍓
 
-Another year older, yet somehow you keep becoming more precious to me.
-Even through a screen, you manage to be my comfort, my chaos, my smile.
+Somehow, you keep becoming more precious to me.
+Even through a screen, you still manage to comfort me and I love spending time with you~.
+And thank you for choosing me and for being so patient and kind.
+I’m really glad I get to be here
+Today isn’t just your birthday
+it’s the day the world was blessed with you.
+wishing you a happy birthday 🌹 🎂.
+i love you so much lets stay always together happy and smiling.  🖤
 
-Today isn’t just your birthday.
-It’s the day the world was blessed with you.
-
-Thank you for existing.
-Thank you for choosing me.
-I love you, always.
 `;
-
 let i = 0;
 
 btn.onclick = () => {
@@ -40,21 +40,3 @@ function typeText() {
     }, 2500);
   }
 }
-
-/* Floating hearts */
-setInterval(() => {
-  const heart = document.createElement("span");
-  heart.textContent = "🖤";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.animationDuration = 6 + Math.random() * 5 + "s";
-  hearts.appendChild(heart);
-
-  setTimeout(() => heart.remove(), 12000);
-}, 300);
-
-/* Mouse parallax */
-document.addEventListener("mousemove", e => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 20;
-  const y = (e.clientY / window.innerHeight - 0.5) * 20;
-  bg.style.transform = `translate(${x}px, ${y}px)`;
-});
